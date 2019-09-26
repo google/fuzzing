d07e7fe8dcc9ad1f8da6fa84d860bf7c6c18ff23
Matt Morehouse <mascasa@google.com>
2019-09-25 17:30:49 -0700

    Move fuzzing tutorial from google/fuzzer-test-suite.

---
+++ tutorial/install-clang.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+CLANG_VERSION=6.0.0
+CLANG_DIR=clang+llvm-$CLANG_VERSION-x86_64-linux-gnu-ubuntu-16.04
+curl http://releases.llvm.org/$CLANG_VERSION/$CLANG_DIR.tar.xz | tar xfJ -
+sudo rm -rf /usr/local/bin/clang* /usr/local/lib/clang
+sudo cp -rf  $CLANG_DIR/bin/*  /usr/local/bin
+sudo cp -rf  $CLANG_DIR/lib/clang  /usr/local/lib
+rm -rf $CLANG_DIR
