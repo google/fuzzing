b937a7bbf67ce00d306e708afd5a10372116e7ba
Matt Morehouse <mascasa@google.com>
2019-09-27 14:29:21 -0700

    Move fuzzing tutorial from google/fuzzer-test-suite. (#16)

---
+++ tutorial/libFuzzer/install-deps.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get --yes install curl subversion screen gcc g++ cmake ninja-build golang autoconf libtool apache2 python-dev pkg-config zlib1g-dev libgcrypt11-dev libgss-dev libssl-dev libxml2-dev ragel nasm libarchive-dev make automake libdbus-1-dev libboost-dev autoconf-archive
