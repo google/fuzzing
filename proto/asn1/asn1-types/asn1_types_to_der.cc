c739d4123838506a6cdd72a62ddd5f020a33c0ef
Danny Halawi <dhalawi@google.com>
2020-07-30 05:19:19 +0000

    adding comments to main file

--- proto/asn1/asn1-types/asn1_types_to_der.cc
+++ proto/asn1/asn1-types/asn1_types_to_der.cc
@@ -5,8 +5,14 @@ namespace asn1_types {
 std::vector<uint8_t> ASN1TypesToDER::EncodeBitString(
     const BitString& bit_string) {
   std::vector<uint8_t> der;
+  // BitString has tag number 3 and can be both constructed and primitive
+  // (X.208, Table 1).
+  // Add one to the length for the unused bits byte.
   EncodeTagAndLength((bit_string.encoding() << 5 | 0x03),
-                     bit_string.val().size(), 0, der);
+                     bit_string.val().size() + 1, 0, der);
+
+  // Encode 0 to indicate that there are no unused bits.
+  // This also acts as EOC if val is empty.
   der.push_back(0x00);
   der.insert(der.end(), bit_string.val().begin(), bit_string.val().end());
   return der;
@@ -14,7 +20,10 @@ std::vector<uint8_t> ASN1TypesToDER::EncodeBitString(
 
 std::vector<uint8_t> ASN1TypesToDER::EncodeInteger(const Integer& integer) {
   std::vector<uint8_t> der;
+  // Integer has tag number 2 (X.208, Table 1) and is always primitive (X.690
+  // (2015), 8.3.1).
   EncodeTagAndLength(0x02, integer.val().size(), 0, der);
+
   der.insert(der.end(), integer.val().begin(), integer.val().end());
   return der;
 }
@@ -24,8 +33,12 @@ std::vector<uint8_t> ASN1TypesToDER::EncodeUTCTime(const UTCTime& utc_time) {
   const google::protobuf::Descriptor* desc = utc_time.GetDescriptor();
   const google::protobuf::Reflection* ref = utc_time.GetReflection();
   for (int i = 0; i < 12; i++) {
+    // UTCTime is encoded like a string so add 0x30 to get ascii character.
     der.push_back(0x30 + ref->GetEnumValue(utc_time, desc->field(i)));
   }
+
+  // The encoding shall terminate with "Z" (ITU-T X.680 | ISO/IEC 8824-1).
+  // UTCTime has tag number 23 (X.208, Table 1).
   if (utc_time.zulu()) {
     der.push_back(0x5a);
     EncodeTagAndLength(0x17, 13, 0, der);
@@ -41,9 +54,13 @@ std::vector<uint8_t> ASN1TypesToDER::EncodeGeneralizedTime(
   const google::protobuf::Descriptor* desc = generalized_time.GetDescriptor();
   const google::protobuf::Reflection* ref = generalized_time.GetReflection();
   for (int i = 0; i < 14; i++) {
+    // GeneralizedTime is encoded like a string so add 0x30 to get ascii
+    // character.
     der.push_back(0x30 + ref->GetEnumValue(generalized_time, desc->field(i)));
   }
+  
   // The encoding shall terminate with "Z" (ITU-T X.680 | ISO/IEC 8824-1).
+  // GeneralizedTime has tag number 24 (X.208, Table 1).
   if (generalized_time.zulu()) {
     der.push_back(0x5a);
     EncodeTagAndLength(0x18, 15, 0, der);
