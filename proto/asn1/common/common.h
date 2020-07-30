6274cb2aad8cb436b103f442d5819f013f535669
Danny Halawi <dhalawi@google.com>
2020-07-30 02:59:52 +0000

    adding functions to header

--- proto/asn1/common/common.h
+++ proto/asn1/common/common.h
@@ -12,13 +12,13 @@ void InsertVariableInt(const size_t value,
                        const size_t pos,
                        std::vector<uint8_t>& der);
 
-void EncodeTagAndLength(const size_t tag,
-                        const size_t len,
-                        const size_t pos,
-                        std::vector<uint8_t>& der);
+void EncodeTag(const bool constructed,
+               const size_t tag_num,
+               const size_t pos,
+               std::vector<uint8_t>& der);
 
-void EncodeDefiniteLength(const size_t actual_len,
-                          const size_t len_pos,
-                          const std::vector<uint8_t>& der);
+void EncodeLength(const size_t len,
+                  const size_t len_pos,
+                  const std::vector<uint8_t>& der);
 
 #endif
