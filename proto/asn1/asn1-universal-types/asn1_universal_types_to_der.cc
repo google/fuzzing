f776bcd1ae3333217329522fc9205533d7a19fc1
Danny Halawi <dhalawi@google.com>
2020-08-04 20:23:27 +0000

    commenting, addressing feedback, using string for timestamp, adding validation to return early if wrongly encoded

--- proto/asn1/asn1-universal-types/asn1_universal_types_to_der.cc
+++ proto/asn1/asn1-universal-types/asn1_universal_types_to_der.cc
@@ -1,107 +1,110 @@
 #include "asn1_universal_types_to_der.h"
-#include <google/protobuf/util/time_util.h>
-#include <time.h>
 
 namespace asn1_universal_types {
 
 void Encode(const BitString& bit_string, std::vector<uint8_t>& der) {
-  // BitString has tag number 3 and can be both constructed and primitive
-  // (X.208, Table 1).
-  // Add one to the length for the unused bits byte.
-  EncodeTagAndLength((bit_string.encoding() << 5 | 0x03),
-                     bit_string.val().size() + 1, der.size(), der);
+  // Save the current size in |tag_len_pos| to place tag and length
+  // after the value is encoded.
+  const size_t tag_len_pos = der.size();
 
-  // Encode 0 to indicate that there are no unused bits.
-  // This also acts as EOC if val is empty.
-  der.push_back(0x00);
-  der.insert(der.end(), bit_string.val().begin(), bit_string.val().end());
+  if (bit_string.val().size()) {
+    der.push_back(bit_string.unused_bits());
+    der.insert(der.end(), bit_string.val().begin(), bit_string.val().end());
+  } else {
+    // If the bitstring is empty, there shall be no subsequent octets,
+    // and the initial octet shall be zero (X.690 (2015), 8.6.2.3).
+    der.push_back(0x00);
+  }
+
+  // BitString has tag number 3 (X.208, Table 1) and is primitive in DER
+  // encoding (A Layman's Guide to a Subset of ASN.1, BER, and DER, 5.4).
+  EncodeTagAndLength(0x03, der.size() - tag_len_pos, tag_len_pos, der);
 }
 
 void Encode(const Integer& integer, std::vector<uint8_t>& der) {
+  // Save the current size in |tag_len_pos| to place tag and length
+  // after the value is encoded.
+  const size_t tag_len_pos = der.size();
+
+  if (integer.val().size()) {
+    der.insert(der.end(), integer.val().begin(), integer.val().end());
+  } else {
+    // Cannot have an empty integer, so use the value 0.
+    der.push_back(0x00);
+  }
+
   // Integer has tag number 2 (X.208, Table 1) and is always primitive (X.690
   // (2015), 8.3.1).
-  EncodeTagAndLength(0x02, integer.val().size(), der.size(), der);
-
-  der.insert(der.end(), integer.val().begin(), integer.val().end());
+  EncodeTagAndLength(0x02, der.size() - tag_len_pos, tag_len_pos, der);
 }
 
 void Encode(const UTCTime& utc_time, std::vector<uint8_t>& der) {
-  const size_t size_before = der.size();
+  // Save the current size in |tag_len_pos| to place tag and length
+  // after the value is encoded.
+  const size_t tag_len_pos = der.size();
 
-  const auto time_stamp = utc_time.time_stamp();
-  time_t rawtime =
-      google::protobuf::util::TimeUtil::TimestampToTimeT(time_stamp);
-  struct tm* ptm;
-  ptm = gmtime(&rawtime);
+  Encode(utc_time.time_stamp(), 13, der);
 
-  const uint16_t years_since_1900 = ptm->tm_year;
-  der.push_back(0x30 + 0x01 + (years_since_1900 / 1000));
-  der.push_back(0x30 + 0x09 + (years_since_1900 / 100));
-  der.push_back(0x30 + (years_since_1900 / 10));
-  der.push_back(0x30 + (years_since_1900 % 10));
+  // Check if encoding was unsucessful
+  if (der.size() == tag_len_pos) {
+    return;
+  }
 
-  const uint8_t months_since_jan = ptm->tm_mon;
-  der.push_back(0x31 + (months_since_jan / 10));
-  der.push_back(0x31 + (months_since_jan % 10));
-
-  const uint8_t day_of_month = ptm->tm_mday;
-  der.push_back(0x30 + (day_of_month / 10));
-  der.push_back(0x30 + (day_of_month % 10));
-
-  const uint8_t hours_since_midnight = ptm->tm_hour;
-  der.push_back(0x30 + (hours_since_midnight / 10));
-  der.push_back(0x30 + (hours_since_midnight % 10));
-
-  const uint8_t minutes_after_hour = ptm->tm_min;
-  der.push_back(0x30 + (minutes_after_hour / 10));
-  der.push_back(0x30 + (minutes_after_hour % 10));
-
-  // The encoding shall terminate with "Z" (X.690 (2015), 11.7.1 | ISO/IEC
-  // 8824-1).
-  der.push_back(0x5a);
-
-  // UTCTime has tag number 23 (X.208, Table 1).
-  EncodeTagAndLength(0x17, size_before - der.size(), der.size(), der);
+  // UTCTime has tag number 23 (X.208, Table 1) and is always primitive
+  // in DER encoding (A Layman's Guide to a Subset of ASN.1, BER, and
+  // DER, 5.17).
+  EncodeTagAndLength(0x17, der.size() - tag_len_pos, tag_len_pos, der);
 }
 
 void Encode(const GeneralizedTime& generalized_time,
             std::vector<uint8_t>& der) {
-  const size_t size_before = der.size();
+  // Save the current size in |tag_len_pos| to place tag and length
+  // after the value is encoded.
+  const size_t tag_len_pos = der.size();
 
-  const auto time_stamp = generalized_time.time_stamp();
-  time_t rawtime =
-      google::protobuf::util::TimeUtil::TimestampToTimeT(time_stamp);
-  struct tm* ptm;
-  ptm = gmtime(&rawtime);
+  Encode(generalized_time.time_stamp(), 15, der);
 
-  const uint16_t years_since_1900 = ptm->tm_year;
-  der.push_back(0x30 + 0x01 + (years_since_1900 / 1000));
-  der.push_back(0x30 + 0x09 + (years_since_1900 / 100));
-  der.push_back(0x30 + (years_since_1900 / 10));
-  der.push_back(0x30 + (years_since_1900 % 10));
+  // Check if encoding was unsucessful
+  if (der.size() == tag_len_pos) {
+    return;
+  }
 
-  const uint8_t months_since_jan = ptm->tm_mon;
-  der.push_back(0x31 + (months_since_jan / 10));
-  der.push_back(0x31 + (months_since_jan % 10));
+  // GeneralizedTime has tag number 24 (X.208, Table 1) and is always primitive
+  // in DER encoding (A Layman's Guide to a Subset of ASN.1, BER, and
+  // DER, 5.17).
+  EncodeTagAndLength(0x18, der.size() - tag_len_pos, tag_len_pos, der);
+}
 
-  const uint8_t day_of_month = ptm->tm_mday;
-  der.push_back(0x30 + (day_of_month / 10));
-  der.push_back(0x30 + (day_of_month % 10));
+void Encode(const google::protobuf::Timestamp& time_stamp,
+            const uint8_t num_fields,
+            std::vector<uint8_t>& der) {
+  std::string iso_date = google::protobuf::util::TimeUtil::ToString(time_stamp);
+  if (iso_date.size() <= 25) {
+    return;
+  }
+  
+  std::string time_str;
+  time_str.reserve(num_fields);
+  // See X.690 (2015), 11.7.5: GeneralizedTime also includes the thousands digit
+  // and hundreds digit of the year to support dates after 2050 by representing
+  // the year with four digits.
+  // See X.690 (2015), 11.8.3: UTCTime represents dates between 1950 and 2050,
+  // so need only use tens and ones digit of the year.
+  // Paritioning the year ensure always valid encodings, i.e. need not
+  // check if the year spans a certain range.
+  time_str +=
+      num_fields == 15 ? iso_date.substr(0, 4) : iso_date.substr(2, 2);  // Year
+  time_str += iso_date.substr(5, 2);   // Month
+  time_str += iso_date.substr(8, 2);   // Day
+  time_str += iso_date.substr(11, 2);  // Hour
+  time_str += iso_date.substr(14, 2);  // Minute
+  time_str += iso_date.substr(17, 2);  // Seconds
+  // See X.690 (2015), 11.7.1 & 11.8.1: Encoding terminates with "Z".
+  time_str += "Z";
 
-  const uint8_t hours_since_midnight = ptm->tm_hour;
-  der.push_back(0x30 + (hours_since_midnight / 10));
-  der.push_back(0x30 + (hours_since_midnight % 10));
-
-  const uint8_t minutes_after_hour = ptm->tm_min;
-  der.push_back(0x30 + (minutes_after_hour / 10));
-  der.push_back(0x30 + (minutes_after_hour % 10));
-
-  // The encoding shall terminate with "Z" (X.690 (2015), 11.7.1 | ISO/IEC
-  // 8824-1).
-  der.push_back(0x5a);
-
-  // GeneralizedTime has tag number 24 (X.208, Table 1).
-  EncodeTagAndLength(0x18, size_before - der.size(), der.size(), der);
+  for (const char c : time_str) {
+    der.push_back(static_cast<uint8_t>(c));
+  }
 }
 
 }  // namespace asn1_universal_types
--- proto/asn1/asn1-universal-types/asn1_universal_types_to_der.h
+++ proto/asn1/asn1-universal-types/asn1_universal_types_to_der.h
@@ -1,29 +1,37 @@
 #ifndef ASN1_UNIVERSAL_TYPES_TO_DER_H_
 #define ASN1_UNIVERSAL_TYPES_TO_DER_H_
 
+#include <google/protobuf/util/time_util.h>
+#include <stdint.h>
+
 #include <vector>
 
 #include "asn1_universal_types.pb.h"
 #include "common.h"
-#include <stdint.h>
 
 namespace asn1_universal_types {
 
-// Encodes |bit_string| according to X.690 (2015), 8.6;
-// returning a vector with the encoded bytes.
+// Appends DER encoded |bit_string| to |der| according to X.690
+// (2015), 8.6.
 void Encode(const BitString& bit_string, std::vector<uint8_t>& der);
 
-// Encodes |integer| according to X.690 (2015), 8.3;
-// returning a vector with the encoded bytes.
+// Appends DER encoded |integer| to |der| according to X.690
+// (2015), 8.3.
 void Encode(const Integer& integer, std::vector<uint8_t>& der);
 
-// Encodes |utc_time| according to X.690 (2015), 11.8;
-// returning a vector with the encoded bytes.
+// Appends DER encoded |utc_time| to |der| according to X.690
+// (2015), 11.8.
 void Encode(const UTCTime& utc_time, std::vector<uint8_t>& der);
 
-// Encodes |generalized_time| according to X.690 (2015), 11.7;
-// returning a vector with the encoded bytes.
+// Appends DER encoded |generalized_time| to |der| according to X.690
+// (2015), 11.7.
 void Encode(const GeneralizedTime& generalized_time, std::vector<uint8_t>& der);
+
+// Appends DER encoded |time_stamp| to |der| where |num_fields| determines
+// which type of ASN.1 Time is encoded according to X.690 (2015), 11.7 & 11.8.
+void Encode(const google::protobuf::Timestamp& time_stamp,
+            const uint8_t num_fields,
+            std::vector<uint8_t>& der);
 
 }  // namespace asn1_universal_types
 
