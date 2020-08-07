f4e1f7da2f33e5f1a4c138a9936402032b56c1fb
Danny Halawi <dhalawi@google.com>
2020-08-07 11:59:45 -0700

    X.509 Certificate Protobuf and Protobuf to DER converter (#57)
    * recreating asn1-pdu
    
    * fixing last bug
    
    * adding headers for README
    
    * adding asn1-types project + common
    
    * making common functions
    
    * adding functions to header
    
    * fixing bugs
    
    * changing function to tag+length
    
    * using common functions for ASN1PDU
    
    * adding comments to header files
    
    * adding comments to main file
    
    * making the name better -> universal types
    
    * correcting spelling of package
    
    * chaning header
    
    * refactoring PDU and editing header name
    
    * editing universal types and adding to common
    
    * removing old asn1-pdu
    
    * using google timestamp protobuf and gmtime
    
    * commenting, addressing feedback, using string for timestamp, adding validation to return early if wrongly encoded
    
    * fixed spelling in comments
    
    * improving function header comments
    
    * included spec for bitsring
    
    * changing last comment and fixing format
    
    * moving everything to asn1-pdu
    
    * changing SetTag
    
    * taking out const for ints
    
    * string requires const
    
    * adding expressions for tags
    
    * writing header description
    
    * changing tag to tag_byte
    
    * adding license, improving speed, and improving checks
    
    * adding comment for asn.1 sequence
    
    * addressing all nits
    
    * formatting
    
    * ading x509 cert protobuf and converter
    
    * adding license, changing header

---
+++ proto/asn1-pdu/x509_certificate_to_der.cc
@@ -0,0 +1,161 @@
+// Copyright 2020 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "x509_certificate_to_der.h"
+
+#include "asn1_pdu_to_der.h"
+#include "common.h"
+
+namespace x509_certificate {
+
+DECLARE_ENCODE_FUNCTION(asn1_pdu::PDU) {
+  // Used to encode PDU for fields that contain them.
+  asn1_pdu::ASN1PDUToDER pdu_to_der;
+  std::vector<uint8_t> der_pdu = pdu_to_der.PDUToDER(val);
+  der.insert(der.end(), der_pdu.begin(), der_pdu.end());
+}
+
+DECLARE_ENCODE_FUNCTION(AlgorithmIdentifierSequence) {
+  // Save the current size in |tag_len_pos| to place sequence tag and length
+  // after the value is encoded.
+  size_t tag_len_pos = der.size();
+
+  Encode(val.object_identifier(), der);
+  Encode(val.parameters(), der);
+
+  // The fields of |algorithm_identifier| are wrapped around a sequence (RFC
+  // 5280, 4.1.1.2).
+  // The current size of |der| subtracted by |tag_len_pos|
+  // equates to the size of the value of |algorithm_identifier|.
+  EncodeTagAndLength(kAsn1Sequence, der.size() - tag_len_pos, tag_len_pos, der);
+}
+
+DECLARE_ENCODE_FUNCTION(SubjectPublicKeyInfoSequence) {
+  // Save the current size in |tag_len_pos| to place sequence tag and length
+  // after the value is encoded.
+  size_t tag_len_pos = der.size();
+
+  Encode(val.algorithm_identifier(), der);
+  Encode(val.subject_public_key(), der);
+
+  // The fields of |subject_public_key_info| are wrapped around a sequence (RFC
+  // 5280, 4.1 & 4.1.2.5).
+  // The current size of |der| subtracted by |tag_len_pos|
+  // equates to the size of the value of |subject_public_key_info|.
+  EncodeTagAndLength(kAsn1Sequence, der.size() - tag_len_pos, tag_len_pos, der);
+}
+
+DECLARE_ENCODE_FUNCTION(TimeChoice) {
+  // The |Time| field either has an UTCTime or a GeneralizedTime (RFC 5280, 4.1
+  // & 4.1.2.5).
+  if (val.has_utc_time()) {
+    return Encode(val.utc_time(), der);
+  }
+  return Encode(val.generalized_time(), der);
+}
+
+DECLARE_ENCODE_FUNCTION(ValiditySequence) {
+  // Save the current size in |tag_len_pos| to place sequence tag and length
+  // after the value is encoded.
+  size_t tag_len_pos = der.size();
+
+  Encode(val.not_before().value(), der);
+  Encode(val.not_after().value(), der);
+
+  // The fields of |Validity| are wrapped around a sequence (RFC
+  // 5280, 4.1 & 4.1.2.5).
+  // The current size of |der| subtracted by |tag_len_pos|
+  // equates to the size of the value of |validity|.
+  EncodeTagAndLength(kAsn1Sequence, der.size() - tag_len_pos, tag_len_pos, der);
+}
+
+DECLARE_ENCODE_FUNCTION(VersionNumber) {
+  // |version| is Context-specific with tag number 0 (RFC 5280, 4.1 & 4.1.2.1).
+  // Takes on values 0, 1 and 2, so only require length of 1 to
+  // encode it (RFC 5280, 4.1 & 4.1.2.1).
+  std::vector<uint8_t> der_version = {kAsn1Constructed, 0x01,
+                                      static_cast<uint8_t>(val)};
+  der.insert(der.end(), der_version.begin(), der_version.end());
+}
+
+DECLARE_ENCODE_FUNCTION(TBSCertificateSequence) {
+  // Save the current size in |tag_len_pos| to place sequence tag and length
+  // after the value is encoded.
+  size_t tag_len_pos = der.size();
+
+  Encode(val.version(), der);
+  Encode(val.serial_number(), der);
+  Encode(val.signature_algorithm(), der);
+  Encode(val.issuer(), der);
+  Encode(val.validity(), der);
+  Encode(val.subject(), der);
+  Encode(val.subject_public_key_info(), der);
+
+  // RFC 5280, 4.1: |issuer_unique_id| and |subject_unique_id|
+  // are only set for v2 and v3 and |extensions| only set for v3.
+  // However, set |issuer_unique_id|, |subject_unique_id|, and |extensions|
+  // independently of the version number for interesting inputs.
+  if (val.has_issuer_unique_id()) {
+    size_t pos_of_tag = der.size();
+    Encode(val.issuer_unique_id(), der);
+    // |issuer_unqiue_id| is Context-specific with tag number 1 (RFC 5280, 4.1
+    // & 4.1.2.8).
+    ReplaceTag(kAsn1ContextSpecific | 0x01, pos_of_tag, der);
+  }
+  if (val.has_subject_unique_id()) {
+    size_t pos_of_tag = der.size();
+    Encode(val.subject_unique_id(), der);
+    // |subject_unqiue_id| is Context-specific with tag number 2 (RFC 5280, 4.1
+    // & 4.1.2.8).
+    ReplaceTag(kAsn1ContextSpecific | 0x02, pos_of_tag, der);
+  }
+  if (val.has_extensions()) {
+    size_t pos_of_tag = der.size();
+    Encode(val.extensions(), der);
+    // |extensions| is Context-specific with tag number 3 (RFC 5280, 4.1
+    // & 4.1.2.8).
+    ReplaceTag(kAsn1ContextSpecific | 0x03, pos_of_tag, der);
+  }
+
+  // The fields of |tbs_certificate| are wrapped around a sequence (RFC
+  // 5280, 4.1 & 4.1.2.5).
+  // The current size of |der| subtracted by |tag_len_pos|
+  // equates to the size of the value of |tbs_certificate|.
+  EncodeTagAndLength(kAsn1Sequence, der.size() - tag_len_pos, tag_len_pos, der);
+}
+
+std::vector<uint8_t> X509CertificateToDER(
+    const X509Certificate& X509_certificate) {
+  // Contains DER encoded X509 Certificate.
+  std::vector<uint8_t> der;
+
+  // Save the current size in |tag_len_pos| to place sequence tag and length
+  // after the value is encoded.
+  size_t tag_len_pos = der.size();
+
+  Encode(X509_certificate.tbs_certificate(), der);
+  Encode(X509_certificate.signature_algorithm(), der);
+  Encode(X509_certificate.signature_value(), der);
+
+  // The fields of |X509_certificate| are wrapped around a sequence (RFC
+  // 5280, 4.1 & 4.1.2.5).
+  // The current size of |der| subtracted by |tag_len_pos|
+  // equates to the size of the value of |X509_certificate|.
+  EncodeTagAndLength(kAsn1Sequence, der.size() - tag_len_pos, tag_len_pos, der);
+  return der;
+}
+
+}  // namespace x509_certificate
---
+++ proto/asn1-pdu/x509_certificate_to_der.h
@@ -0,0 +1,60 @@
+// Copyright 2020 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef PROTO_ASN1_PDU_X509_CERTIFICATE_TO_DER_H_
+#define PROTO_ASN1_PDU_X509_CERTIFICATE_TO_DER_H_
+
+#include <stdint.h>
+
+#include <vector>
+
+#include "asn1_universal_types_to_der.h"
+#include "x509_certificate.pb.h"
+
+namespace x509_certificate {
+
+// Encodes |X509_certificate| to DER, returning the encoded bytes in |der_|.
+std::vector<uint8_t> X509CertificateToDER(
+    const X509Certificate& X509_certificate);
+
+// Encodes a |pdu| if |t| contains one; otherwise, encodes the value belonging
+// to |t|.
+template <typename T>
+void Encode(const T& t, std::vector<uint8_t>& der) {
+  if (t.has_pdu()) {
+    Encode(t.pdu(), der);
+    return;
+  }
+  Encode(t.value(), der);
+}
+
+// Encodes the |TYPE| found in X509 Certificates and writes the results to
+// |der|.
+#define DECLARE_ENCODE_FUNCTION(TYPE) \
+  template <>                         \
+  void Encode<TYPE>(const TYPE& val, std::vector<uint8_t>& der)
+
+DECLARE_ENCODE_FUNCTION(TBSCertificateSequence);
+DECLARE_ENCODE_FUNCTION(VersionNumber);
+DECLARE_ENCODE_FUNCTION(ValiditySequence);
+DECLARE_ENCODE_FUNCTION(TimeChoice);
+DECLARE_ENCODE_FUNCTION(SubjectPublicKeyInfoSequence);
+DECLARE_ENCODE_FUNCTION(AlgorithmIdentifierSequence);
+DECLARE_ENCODE_FUNCTION(asn1_pdu::PDU);
+
+}  // namespace x509_certificate
+
+#endif  // PROTO_ASN1_PDU_X509_CERTIFICATE_TO_DER_H_
