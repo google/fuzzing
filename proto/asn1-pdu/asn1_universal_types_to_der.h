c264e768815c6665720e32df256369d8e546d747
Danny Halawi <dhalawi@google.com>
2020-08-13 11:48:24 -0700

    [proto] ASN.1 Object Identifier (#63)
    * adding oid
    
    * nit: format
    
    * fixing comments
    
    * using base 128 encode and addressing duplicate first index in subidentfier

--- proto/asn1-pdu/asn1_universal_types_to_der.h
+++ proto/asn1-pdu/asn1_universal_types_to_der.h
@@ -37,6 +37,11 @@ void Encode(const Integer& integer, std::vector<uint8_t>& der);
 // Appends encoded |bit_string| to |der|.
 void Encode(const BitString& bit_string, std::vector<uint8_t>& der);
 
+// DER encodes |object_identifier| according to X.690 (2015), 8.19.
+// Appends encoded |object_identifier| to |der|.
+void Encode(const ObjectIdentifier& object_identifier,
+            std::vector<uint8_t>& der);
+
 // DER encodes |utc_time| according to X.690 (2015), 11.8.
 // Appends encoded |utc_time| to |der|.
 void Encode(const UTCTime& utc_time, std::vector<uint8_t>& der);
