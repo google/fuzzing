322bf3bbf57e6a8424cbd4619bb1c885e449dc23
leakhena8300 <95209689+leakhena8300@users.noreply.github.com>
2021-11-29 10:27:44 +0100

    Update
    1st version

--- tutorial/libFuzzer/fuzz_me.cc
+++ tutorial/libFuzzer/fuzz_me.cc
@@ -10,6 +10,10 @@ bool FuzzMe(const uint8_t *Data, size_t DataSize) {
 }
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-  FuzzMe(Data, Size);
+  if (size > 0 && data[0] == 'H')
+    if (size > 1 && data[1] == 'I')
+       if (size > 2 && data[2] == '!')
+       __builtin_trap();
+
   return 0;
 }
